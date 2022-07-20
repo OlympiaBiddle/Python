b9a06dd2fa72775e4ebd35bac3ec9b18aa9fc9b1
OlympiaBiddle <osb399@gmail.com>
2022-07-20 16:25:17 -0400

    learning functions

---
+++ Numbers.py
@@ -0,0 +1,21 @@
+from math import *
+
+print(2)
+print(2*(3 +5))
+print(3+4.8)
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+print(str(my_num) + " my favorite number")
+
+new_num = -5
+print(abs(new_num))
+print(pow(25, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.7))
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
+
---
+++ text_strings.py
@@ -0,0 +1,14 @@
+phrase = "Giraffe Academy"
+
+print(phrase)
+print(len(phrase))
+print(phrase[2])
+print(phrase.index("m"))
+print(phrase.replace("Giraffe", "Elephant"))
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(phrase.lower())
+print(phrase.upper())
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
+print(phrase + " is cool")
